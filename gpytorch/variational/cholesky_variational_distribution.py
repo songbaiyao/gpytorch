cc75c9c23aa0c6a191430223a95a6e9d0a74a6ee
Geoff Pleiss <gpleiss@gmail.com>
2019-01-19 15:35:14 -0500

    Stable initialization

--- gpytorch/variational/cholesky_variational_distribution.py
+++ gpytorch/variational/cholesky_variational_distribution.py
@@ -35,7 +35,9 @@ class CholeskyVariationalDistribution(VariationalDistribution):
 
     def initialize_variational_distribution(self, prior_dist):
         self.variational_mean.data.copy_(prior_dist.mean)
-        self.chol_variational_covar.data.copy_(prior_dist.covariance_matrix.inverse().cholesky())
+        self.chol_variational_covar.data.copy_(
+            prior_dist.covariance_matrix.double().inverse().cholesky().type_as(prior_dist.mean)
+        )
 
     @property
     def variational_distribution(self):
